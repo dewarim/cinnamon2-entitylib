7fefaa2a4e1ad52e14041d7446b9d8bdcb29df01
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-04-19 17:27:26 +0200

    Added filter method for lifecycle events.

--- src/server/audit/AuditService.java
+++ src/server/audit/AuditService.java
@@ -1,12 +1,19 @@
 package server.audit;
 
+import org.dom4j.Document;
+import org.dom4j.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import server.ConfigEntry;
 import server.User;
+import server.dao.ConfigEntryDAO;
+import server.dao.DAOFactory;
 import server.data.ObjectSystemData;
+import server.exceptions.CinnamonConfigurationException;
 import server.lifecycle.LifeCycle;
 import server.lifecycle.LifeCycleState;
 import utils.HibernateSession;
+import utils.ParamParser;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -30,6 +37,10 @@ public class AuditService {
         try {
             if(connection == null){
                 log.debug("no audit connection available");
+                return;
+            }
+            if(event == null){
+                log.debug("null event - filtered.");
                 return;
             }
             PreparedStatement stmt =
@@ -65,6 +76,28 @@ public class AuditService {
     }
     
     public LogEvent createLogEvent(ObjectSystemData osd, User user, LifeCycleState oldState, LifeCycleState nextState){
+        if(oldState == null && nextState == null){
+            log.debug("both lifecycle states are null - nothing to do.");
+            return null;
+        }
+        // This would be much simpler with Groovy and ?. / ?:
+        Long oldLc = 0L;
+        Long newLc = 0L;        
+        Long oldStateId = 0L;
+        Long newStateId = 0L;
+        if(oldState != null){
+            oldStateId = oldState.getId();
+            oldLc = oldState.getLifeCycle().getId();
+        }
+        if(nextState != null){
+            newStateId = nextState.getLifeCycle().getId();
+            newLc = nextState.getId();
+        }        
+        if(filterLogMessageByLifecycle(oldLc, newLc, oldStateId, newStateId)){
+            log.debug("Lifecycle log message has been filtered.");
+            return null;
+        }
+        
         LogEvent event = new LogEvent(osd);
         event.setRepositoryName(HibernateSession.getLocalRepositoryName());
         event.setFieldName("state");
@@ -79,7 +112,7 @@ public class AuditService {
             event.setNewValueName(nextState.getName());
             LifeCycle nextLc = nextState.getLifeCycle();
            
-            if(nextLc != null){
+            if(nextLc != null){ // contrary to IntelliJ's opinion, this *may* be null in a mis-configured repo.
                 lifecycleName = nextLc.getName();
             }
         }        
@@ -112,4 +145,42 @@ public class AuditService {
         return event;
     }
     
+    /**
+     * Determine whether a log message should be filtered, depending on the ChangeTrigger's configuration.
+     * A node "logEverything" with content "true" will approve all log messages.
+     * @param oldLifecycleId the old lifecycle's id
+     * @param newLifecycleId the new lifecycle's id
+     * @param oldStateId the old lifecycle state id , may be null or empty
+     * @param newStateId the new lifecycle state id, may be null or empty
+     * @return true if the log message should be filtered (not stored in the database), false otherwise.
+     */
+    Boolean filterLogMessageByLifecycle(Long oldLifecycleId, Long newLifecycleId, Long oldStateId, Long newStateId ){
+        DAOFactory daoFactory = DAOFactory.instance(DAOFactory.HIBERNATE);
+        ConfigEntryDAO ceDao = daoFactory.getConfigEntryDAO(HibernateSession.getLocalEntityManager());
+        ConfigEntry config = ceDao.findByName("audit.trail.filter");
+        if(config == null){
+            throw new CinnamonConfigurationException("Configuration entry audit.trail.filter is missing.");
+        }
+        // only log configured changes:        
+        Document filterConfig = ParamParser.parseXmlToDocument(config.getConfig());
+        Node logAll = filterConfig.selectSingleNode("//logEverything[text()='true']");
+        if(logAll != null){
+            log.debug("Found logEverything directive: approve log message");
+            return false;
+        }
+        
+        Node lifecycle = filterConfig.selectSingleNode("//lifecycles/lifecycle[@id='"+oldLifecycleId+"' or @id='"+newLifecycleId+"']");
+        if(lifecycle == null){
+            log.debug("Lifecycle is not configured for logging: deny logging.");
+            return true;
+        }
+        Node oldState = lifecycle.selectSingleNode("stateId[text()='"+oldStateId+"']");
+        Node newState = lifecycle.selectSingleNode("stateId[text()='"+newStateId+"']");
+        if(oldState != null || newState != null){
+            log.debug("Lifecycle state change will be logged.");
+            return false;
+        }
+        log.debug("Could not find a reason to approve this log event.");
+        return true;
+    }
 }
